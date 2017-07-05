15a5b446abbd00b46a487e491302d20d96bf1011
James <boydston.j@gmail.com>
2017-07-05 14:53:45 -0700

    looks done

--- main.js
+++ main.js
@@ -3,25 +3,25 @@
 // 1.
 // What does `givenName` equal right now?
 let givenName;
-// A:
+// A: givenName is a undefined variable
 
 
 // 2.
 // What is `givenName` set to right now?
 givenName = "Tim";
-// A:
+// A: givenName is now equal to "Tim"
 
 
 // 3.
 // What is `givenName` set to right now?
 givenName = givenName;
-// A:
+// A: If givenName was just set to "Tim", then givenName equals itself and is still equal to "Tim".
 
 
 // 4.
 // What is `greeting` set to?
 let greeting = "Hello, how are you? " + givenName;
-// A:
+// A: greeting is set to "Hello, how are you? " + "Tim"
 
 
 // 5.
@@ -29,13 +29,13 @@ let greeting = "Hello, how are you? " + givenName;
 let high = 50;
 let low  = 10;
 let math = high - low;
-// A:
+// A: math is set to (high - low), where high is set to 50 and low is set to 10, then math is set to (50 - 10).
 
 
 // 6.
 // What is `math` set to?
 math = high - "5";
-// A:
+// A: math is set to high minus the string "5". I am not sure how js handles this.
 
 
 // 7.
@@ -45,19 +45,21 @@ math = high - "5";
 let born = 1984;
 let today = 2017;
 // A:
+let age = givenName + " is " + (today - born) + " years old"
 
 
 // 8.
 // Adjust this code. Store some information in the following variables.
 // A:
-let yourName;
-let instructorName;
+let yourName = "Square"
+let instructorName = "OverTheMike"
 
 
 // 9.
 // Update the variables `yourName` and `instructorName` so the expression below reads correctly.
 // A:
-
+yourName = "James"
+instructorName = "Mike"
 
 // This statement should read correctly
 let statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
@@ -66,11 +68,11 @@ let statement = yourName + " is taking a class at The Iron Yard, my instructor's
 // 10.
 // We've created a variable named after each primitive type.
 // Use the `=` operator to assign a value to each variable matching the type in the name.
-let myNumber;
-let myString;
-let myBoolean;
+let myNumber = 5
+let myString = "SquaredDev"
+let myBoolean = true
 // This one's a little tricky - think carefully about what makes a value "undefined"!
-let myUndefined;
+let myUndefined
 // A:
 
 
@@ -78,47 +80,47 @@ let myUndefined;
 
 // 11.
 let x = (false == 0);
-// A.
+// A. True. If the initial value is false, then any statement following it is true.
 
 
 // 12.
 let y = (false == "");
-// A.
+// A. True. If the initial value is false, then any statement following it is true.
 
 
 // 13.
 let z = (0 == "");
-// A.
+// A. True
 
 
 // 14.
 let a = (null == null);
-// A.
+// A. True.
 
 
 // 25.
 let b = (undefined == undefined);
-// A.
+// A. True
 
 
 // 16.
 let c = (undefined == null);
-// A.
+// A. True
 
 
 // 17.
 let d = (null == false);
-// A.
+// A. True
 
 
 // 18.
 let e = (NaN == null);
-// A.
+// A. True
 
 
 // 19.
 let f = (NaN == NaN);
-// A.
+// A. True
 
 
 // 20.
@@ -127,6 +129,7 @@ let f = (NaN == NaN);
 // A:
 let thirsty = true;
 
+if (thirsty)
 {
   console.log("I'm parched!");
 } else {
@@ -135,15 +138,15 @@ let thirsty = true;
 
 // For questions 21 - 29 list which statement will be logged.
 
-// 21.
-let x;
+// // 21.
+x = true;
 
 if(x){
 	console.log("True!");
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 22.
@@ -152,19 +155,19 @@ if(""){
 } else {
 	console.log("False!");
 }
-// A:
+// A: false
 
 
 // 23.
 // this expression will set x to NaN
-let x = 1 * undefined;
+x = 1 * undefined;
 
 if(x){
 	console.log("True!");
 } else {
 	console.log("False!");
 }
-// A:
+// A: false
 
 
 // 24.
@@ -173,7 +176,7 @@ if(false){
 } else {
 	console.log("False!");
 }
-// A:
+// A: false
 
 
 // 25.
@@ -182,7 +185,7 @@ if(-5 && 5){
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 26.
@@ -191,7 +194,7 @@ if("Hello World!"){
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 27.
@@ -200,7 +203,7 @@ if({}){
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 28.
@@ -209,7 +212,7 @@ if({name: "Yehuda Katz"}){
 } else {
 	console.log("False!");
 }
-// A:
+// A: true
 
 
 // 29.
